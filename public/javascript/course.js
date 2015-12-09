b4fd86fa32906cd4f6491c1a575c442e84db4271
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-12-08 22:23:50 -0800

    par and yardages added to performance summary by hole

--- public/javascript/course.js
+++ public/javascript/course.js
@@ -80,9 +80,19 @@
 
                 // Calculating MEAN score //
                 var Rounds = requiredGames.length;
+                $('#noOfRounds').append(Rounds);
                 var totalGross = 0;
                 var totalPutts = 0;
                 var totalNett = 0;
+
+                var mostRecentGame = requiredGames[0].date;
+                for (i = 0; i < requiredGames.length; i++) {
+                    if (requiredGames[i].date > mostRecentGame) {
+                        mostRecentGame = requiredGames[i].date;
+                    }
+                }
+                $('#lastGameDate').append(mostRecentGame);
+
 
                 for (i = 0; i < requiredGames.length; i++) {
                     totalGross += requiredGames[i].totalScore;
@@ -110,8 +120,89 @@
                         type: 'GET',
                         datatype: 'json',
                         success: function(data) {
+                              console.log(data);
                               var name = data[0].coursename;
                               $('#courseName').append(name);
+                              $('#par1').text(data[0].par[0]);
+                              $('#par2').text(data[0].par[1]);
+                              $('#par3').text(data[0].par[2]);
+                              $('#par4').text(data[0].par[3]);
+                              $('#par5').text(data[0].par[4]);
+                              $('#par6').text(data[0].par[5]);
+                              $('#par7').text(data[0].par[6]);
+                              $('#par8').text(data[0].par[7]);
+                              $('#par9').text(data[0].par[8]);                                                                                                                                                      
+                              $('#par10').text(data[0].par[9]);
+                              $('#par11').text(data[0].par[10]);
+                              $('#par12').text(data[0].par[11]);
+                              $('#par13').text(data[0].par[12]);
+                              $('#par14').text(data[0].par[13]);
+                              $('#par15').text(data[0].par[14]);
+                              $('#par16').text(data[0].par[15]);
+                              $('#par17').text(data[0].par[16]);
+                              $('#par18').text(data[0].par[17]);
+
+                              front9ParArray = [];
+                              for (i = 0; i < 8; i++) {
+                                  front9ParArray.push(data[0].par[i]);
+                              }
+                              front9Par = front9ParArray.reduce(function (previousValue, currentValue, currentIndex, array) {
+                                  return previousValue + currentValue;
+                              });
+                              console.log(front9Par);
+                              $('#front9par').text(front9Par);
+
+
+                              back9ParArray = [];
+                              for (i = 0; i < 8; i++) {
+                                  back9ParArray.push(data[0].par[i]);
+                              }
+                              back9Par = back9ParArray.reduce(function (previousValue, currentValue, currentIndex, array) {
+                                  return previousValue + currentValue;
+                              });
+                              console.log(back9Par);
+                              $('#back9par').text(back9Par);
+
+
+                              // Assign yardages to average hole performance //
+                              $('#yardage1').text(data[0].yardage[0]);
+                              $('#yardage2').text(data[0].yardage[1]);
+                              $('#yardage3').text(data[0].yardage[2]);
+                              $('#yardage4').text(data[0].yardage[3]);
+                              $('#yardage5').text(data[0].yardage[4]);
+                              $('#yardage6').text(data[0].yardage[5]);
+                              $('#yardage7').text(data[0].yardage[6]);
+                              $('#yardage8').text(data[0].yardage[7]);
+                              $('#yardage9').text(data[0].yardage[8]);                                                                                                                                                      
+                              $('#yardage10').text(data[0].yardage[9]);
+                              $('#yardage11').text(data[0].yardage[10]);
+                              $('#yardage12').text(data[0].yardage[11]);
+                              $('#yardage13').text(data[0].yardage[12]);
+                              $('#yardage14').text(data[0].yardage[13]);
+                              $('#yardage15').text(data[0].yardage[14]);
+                              $('#yardage16').text(data[0].yardage[15]);
+                              $('#yardage17').text(data[0].yardage[16]);
+                              $('#yardage18').text(data[0].yardage[17]);
+
+                              front9YardageArray = [];
+                              for (i = 0; i < 8; i++) {
+                                  front9YardageArray.push(data[0].yardage[i]);
+                              }
+                              front9Yardage = front9YardageArray.reduce(function (previousValue, currentValue, currentIndex, array) {
+                                  return previousValue + currentValue;
+                              });
+                              $('#front9yardage').text(front9Yardage);
+
+
+                              back9YardageArray = [];
+                              for (i = 0; i < 8; i++) {
+                                  back9YardageArray.push(data[0].yardage[i]);
+                              }
+                              back9Yardage = back9YardageArray.reduce(function (previousValue, currentValue, currentIndex, array) {
+                                  return previousValue + currentValue;
+                              });
+                              $('#back9yardage').text(back9Yardage);
+
                         }
                 });
             }
